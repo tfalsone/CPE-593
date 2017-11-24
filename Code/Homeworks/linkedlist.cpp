992be74323e1b05ec490e40e94967cf8fa75c511
tfalsone <thomas.falsone@gmail.com>
2017-11-24 11:43:41 -0500

    linkedlist updates

--- Code/Homeworks/linkedlist.cpp
+++ Code/Homeworks/linkedlist.cpp
@@ -9,10 +9,11 @@ using namespace std;
 class LL2 {
 private:
     class Node {
+    public:
         Node* next;
         Node* last;
         int val;
-        Node(int v, Node *next, Node *last) : v(val), next(next), last(last);
+        Node(int val, Node *next, Node *last) : val(val), next(next), last(last) {};
     } *head, *tail;
 
 public:
@@ -30,16 +31,20 @@ public:
             tail = head;
         } else {
             Node* temp = new Node(v, head, nullptr);
+            head->last = temp;
             head = temp;
         }
     }
 
     void addAtTail(int v) {
-        if(tail = nullptr) {
+        if(tail == nullptr) {
+            //cout << "add first element" << endl;
             head = new Node(v, nullptr, nullptr);
             tail = head;
         } else {
+            //cout << "add at tail" << endl;
             Node* temp = new Node(v, nullptr, tail);
+            tail->next = temp;
             tail = temp;
         }
     }
@@ -54,7 +59,6 @@ public:
                 head = head->next;
                 delete temp;
             } else {
-                temp = head;
                 head = tail = nullptr;
                 delete temp;
             }
@@ -62,6 +66,122 @@ public:
     }
 
     void deleteAtTail() {
-        
+        if(tail == nullptr) {
+            cout << "This list is empty, you can not delete from it" << endl;
+            exit(1);
+        } else {
+            Node* temp;
+            if(head->next != nullptr) { // Only one element in list
+                //cout << "deleting element" << endl;
+                temp = tail;
+                tail = tail->last;
+                tail->next = nullptr;
+                delete temp;
+            } else {                
+                temp = head;
+                head = tail = nullptr;
+                delete temp;
+            }
+        }
+    }
+
+    void addFront(int start, int step, int end) {
+        if(step == 0) { return; }
+
+        for(int i = start; i <= end; i += step) {
+            addAtHead(i);
+        }
+    }
+
+    void addBack(int start, int step, int end) {
+        //cout << "start add" << endl;
+        if(step == 0) { return; }
+
+        for(int i = start; i <= end; i += step) {
+            addAtTail(i);
+        }
+    }
+
+    void removeFront(int n) {
+        for (int i = 0; i < n; i++) {
+            deleteAtHead();
+        }
+    }
+
+    void removeBack(int n) {
+        for (int i = 0; i < n; i++) {
+            deleteAtTail();
+        }
+    }
+
+    void output() {
+        if(head == nullptr) { return; }
+        Node* temp = head;
+        while(temp != nullptr) {
+            cout << temp->val << ",";
+            temp = temp->next;
+        }
+        cout << '\n';
     }
 };
+
+int main() {
+    LL2 arr;
+
+    ifstream infile("HW4b.txt");
+    string line;
+
+    while (getline(infile, line))
+    {
+        stringstream iss(line);
+        string inst, data;
+        iss >> inst >> data;
+
+        if (inst == "ADD_FRONT")
+        {
+            stringstream ss(data);
+            vector<int> vect;
+            int i;
+            while (ss >> i)
+            {
+                vect.push_back(i);
+
+                if (ss.peek() == ':')
+                    ss.ignore();
+            }
+            arr.addFront(vect[0], vect[1], vect[2]);
+        }
+        else if (inst == "ADD_BACK")
+        {
+            stringstream ss(data);
+            vector<int> vect;
+            int i;
+            while (ss >> i)
+            {
+                vect.push_back(i);
+
+                if (ss.peek() == ':')
+                    ss.ignore();
+            }
+            arr.addBack(vect[0], vect[1], vect[2]);
+        }
+        else if (inst == "OUTPUT")
+        {
+            arr.output();
+        }
+        else if (inst == "REMOVE_FRONT")
+        {
+            int n = stoi(data);
+            arr.removeFront(n);
+        }
+        else if (inst == "REMOVE_BACK")
+        {
+            int n = stoi(data);
+            arr.removeBack(n);
+        }
+        else
+        {
+            cout << "Error reading instruction" << endl;
+        }
+    }
+}
